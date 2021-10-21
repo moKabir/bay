eaaca7338ea7fd8bd01393418351a2de20e2a7a7
moKabir <64577345+moKabir@users.noreply.github.com>
2021-10-21 15:53:27 +0530

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,33 @@
+/* Navigation */
+jQuery(document).ready(function(e) {
+
+	jQuery(".humburger").click(function() {
+        jQuery("body").addClass("menu-action");
+    });
+	jQuery(".close").click(function() {
+        jQuery("body").removeClass("menu-action");
+		jQuery("body").removeClass("popup-action");
+		jQuery(this).parent().parent().removeClass("show active");
+    });
+	
+	jQuery(".main-menu").click(function() {
+        jQuery("body").addClass("main-menu-action");
+    });
+	jQuery(".close").click(function() {
+        jQuery("body").removeClass("main-menu-action");
+    });
+	
+});
+
+//jQuery(window).on('click touchend', function(e) {
+//	if (!jQuery(e.target).hasClass("humburger") && !jQuery(e.target).hasClass("close") && !jQuery(e.target).hasClass("d-menu") && jQuery(e.target).parents(".d-menu").length === 0) {
+//        jQuery("body").removeClass("menu-action");
+//	}
+//});
+//jQuery(window).on('click touchend', function(e) {
+//	if (!jQuery(e.target).hasClass("main-menu") && !jQuery(e.target).hasClass("close") && !jQuery(e.target).hasClass("naviagtion") && jQuery(e.target).parents(".navigation").length === 0) {
+//        jQuery("body").removeClass("main-menu-action");
+//	}
+//});
+
+/* Navigation */
