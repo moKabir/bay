f318f9c2b2ffc1b94296100456ee22f0c75cd0a7
moKabir <64577345+moKabir@users.noreply.github.com>
2021-10-27 16:12:41 +0530

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -19,15 +19,16 @@ jQuery(document).ready(function(e) {
 	
 });
 
-//jQuery(window).on('click touchend', function(e) {
-//	if (!jQuery(e.target).hasClass("humburger") && !jQuery(e.target).hasClass("close") && !jQuery(e.target).hasClass("d-menu") && jQuery(e.target).parents(".d-menu").length === 0) {
-//        jQuery("body").removeClass("menu-action");
-//	}
-//});
-//jQuery(window).on('click touchend', function(e) {
-//	if (!jQuery(e.target).hasClass("main-menu") && !jQuery(e.target).hasClass("close") && !jQuery(e.target).hasClass("naviagtion") && jQuery(e.target).parents(".navigation").length === 0) {
-//        jQuery("body").removeClass("main-menu-action");
-//	}
-//});
+jQuery(window).on('click touchend', function(e) {
+	if (!jQuery(e.target).hasClass("humburger") && !jQuery(e.target).hasClass("close") && !jQuery(e.target).hasClass("d-menu") && jQuery(e.target).parents(".d-menu").length === 0 && jQuery(e.target).parents(".humburger").length === 0) {
+        jQuery("body").removeClass("menu-action");
+	}
+});
+
+jQuery(window).on('click touchend', function(e) {
+	if (!jQuery(e.target).hasClass("main-menu") && !jQuery(e.target).hasClass("close") && !jQuery(e.target).hasClass("navigation") && jQuery(e.target).parents(".navigation").length === 0 && jQuery(e.target).parents(".main-menu").length === 0) {
+        jQuery("body").removeClass("main-menu-action");
+	}
+});
 
 /* Navigation */
